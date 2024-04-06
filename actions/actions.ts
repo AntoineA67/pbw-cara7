6d5877708bb381c6cd6f927d88da57edb05304ce
Antoine Adamy <antoine.adamy@gmail.com>
2024-04-06 19:33:04 +0200

    oui

--- actions/actions.ts
+++ actions/actions.ts
@@ -1,6 +1,10 @@
 'use server'
 import prisma from '@/lib/prisma'
 
+export async function getUsers() {
+	return prisma.user.findMany();
+}
+
 export async function create() {
 	prisma.user.create({
 		data: {
--- app/ui/Buttons.tsx
+++ app/ui/Buttons.tsx
@@ -1,9 +1,16 @@
 'use client';
 import { Button } from '@mui/material';
-import { create } from '@/actions/actions';
+import { create, getUsers } from '@/actions/actions';
 
 export function TestButton() {
 	return (
 		<button onClick={() => create()}>Test</button>
 	);
+}
+
+export function TestButton2() {
+	const users = getUsers();
+	return (
+		{ users }
+	)
 }
